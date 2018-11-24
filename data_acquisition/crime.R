3cf6f47acbe41039f10bc151f6862c0dd1b01fa0
J <jude.canady@gmail.com>
2018-11-24 13:00:58 -0600

    num crimes per day

--- data_acquisition/crime.R
+++ data_acquisition/crime.R
@@ -18,9 +18,19 @@ query <- "$where=offense_date between '2011-01-01' and '2018-11-21'"
 crimeIncidents <-read.socrata(paste0(crimeIncidents, query), app_token = token[['app']])
 crimeIncidents <- as_tibble(crimeIncidents)
 
+crimeIncidents <- crimeIncidents %>% 
+  mutate(geolocation = str_extract_all(geolocation, '[-,.,0-9]+')) %>% 
+  mutate(long = map_chr(geolocation, 1), lat = map_chr(geolocation, 2)) %>% 
+  mutate_at(vars(long, lat), as.double)
+
 #all ofenses by day
 num_offenses <- crimeIncidents %>% group_by(offense_date) %>% summarise(num_crimes = n())
 num_offenses$offense_date <- as.Date(num_offenses$offense_date)
+
+ggplot() +
+  geom_line(data=num_offenses, aes(x=offense_date, y=num_crimes)) +
+  ylab("Number of Crimes per Day") +
+  xlab("Offense Date")
 
 # create file with all offenses
 num_offenses <- inner_join(weather_data, num_offenses, by=c("year_month_day"="offense_date"))
